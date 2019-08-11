f7f0a70a8c53cb10c8e2ec8cb847d96c722c8a35
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-08-11 15:58:20 +1200

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","","assignment1");   //query to create connection
-if (mysqli_connect_errno())                        //check if there is an error
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-/*else{ echo "connected"; }*/
-?>
