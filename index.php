67b366303cbdc10792ea24b699af23529fa29ed5
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-08-12 01:17:25 +1200

    welcome page
    show some links and user welcome message.

--- index.php
+++ index.php
@@ -1,20 +1,30 @@
 <?php
+require('db_connect.php');
 session_start();
-if(!isset($_SESSION["fullname"])){     // check fullname is there
-	header("Location: login.php");   // redirect location
-	exit(); //end of condition
+if(!isset($_SESSION["email"])){
+	header("Location: login.php");
+	exit(); 
 }
+$email = $_SESSION["email"];
+
+$sql1 = "SELECT * FROM users WHERE email='$email'";
+$result1 = mysqli_query($con, $sql1);
+$username = mysqli_fetch_assoc($result1)['fullname'];
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Welcome User</title>
-<link rel="stylesheet" href="style.css" />   //add style.css file for designing purpose
+<link rel="stylesheet" href="style.css" />
 </head>
 <body>
 <div class="welcome">
-<h1 >Welcome <?php echo $_SESSION['fullname']; ?>!</h1>
-<a href="logout.php">Logout</a>
+<h1 >Welcome <?php echo $username; ?>! </h1>
+<ul class="account_list">
+<li><a href="logout.php">Logout</a></li>
+<li><a href="reset_password.php">Reset Password</a></li>
+</ul>
+
 </div>
 
 </body>
