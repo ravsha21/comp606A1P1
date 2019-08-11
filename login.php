ecb7eff9dbff093f4387fffe2a7f9c035a704e6f
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-08-12 01:19:15 +1200

    login area
    add classes to design the login form.

--- login.php
+++ login.php
@@ -8,17 +8,17 @@
 <?php
 session_start();
 require('db_connect.php');
-if (isset($_POST['fullname'])){
-	$fullname = stripslashes($_POST['fullname']);
-	$fullname = mysqli_real_escape_string($con,$fullname);
+if (isset($_POST['email'])){
+	$email = stripslashes($_POST['email']);
+	$email = mysqli_real_escape_string($con,$email);
 	$password = stripslashes($_POST['password']);
 	$password = mysqli_real_escape_string($con,$password);
-        $query = "SELECT * FROM users WHERE fullname='$fullname'
+        $query = "SELECT * FROM users WHERE email='$email'
 and password='".md5($password)."'";
 	$result = mysqli_query($con,$query) or die(mysql_error());
 	$rows = mysqli_num_rows($result);
         if($rows==1){
-	    $_SESSION['fullname'] = $fullname;
+	    $_SESSION['email'] = $email;
 	    header("Location: index.php");
          }else{
 	echo "<div class='form'>
@@ -27,12 +27,12 @@ and password='".md5($password)."'";
 	}
     }else{
 ?>
-	<form class="login_here" action="" method="post" name="login">
+	<form class="login_form" action="" method="post" name="login">
     <h1 class="login-here">Login</h1>
-    <input type="text" class="login-field" name="fullname" placeholder="Fullname" autofocus><br>
-    <input type="password" class="login-field" name="password" placeholder="Password"><br>
-    <input type="submit" value="Login" name="submit" class="loginButton">
-  <p class="registration-here"><a href="registration.php">Register</a></p>
+    <input type="text" class="login-field" name="email" placeholder="Email address" autofocus required><br>
+    <input type="password" class="login-field" name="password" placeholder="Password" required><br>
+<input type="submit" value="Login" name="submit" class="loginButton">
+<p class="registration-here"><a href="registration.php">Register</a></p>
   </form>
  
 <?php } ?>
