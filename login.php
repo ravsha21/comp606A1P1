6cda331e632f9b62fc6f9e9e93a53cef065838a6
ravsha21 <52937253+ravsha21@users.noreply.github.com>
2019-08-12 12:08:46 +1200

    user login
    Add some comments to represent the functionality.

--- login.php
+++ login.php
@@ -6,21 +6,20 @@
 </head>
 <body>
 <?php
-session_start();
-require('db_connect.php');
-if (isset($_POST['email'])){
-	$email = stripslashes($_POST['email']);
-	$email = mysqli_real_escape_string($con,$email);
+session_start();     
+require('db_connect.php');      //include database file
+if (isset($_POST['email'])){   
+	$email = stripslashes($_POST['email']);   //function to removes backslashes
+	$email = mysqli_real_escape_string($con,$email);   //to avoid sql injection
 	$password = stripslashes($_POST['password']);
 	$password = mysqli_real_escape_string($con,$password);
-        $query = "SELECT * FROM users WHERE email='$email'
-and password='".md5($password)."'";
+        $query = "SELECT * FROM users WHERE email='$email' and password='".md5($password)."'"; //get data from user table
 	$result = mysqli_query($con,$query) or die(mysql_error());
 	$rows = mysqli_num_rows($result);
         if($rows==1){
-	    $_SESSION['email'] = $email;
+	    $_SESSION['email'] = $email;  //store value in session
 	    header("Location: index.php");
-         }else{
+         }else{           //if record not  match
 	echo "<div class='form'>
 <h3>Username/password is incorrect.</h3>
 <br/>Click here to <a href='login.php'>Login</a></div>";
